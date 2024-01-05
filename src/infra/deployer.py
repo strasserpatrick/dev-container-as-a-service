e374c5c958851e90f94209990703131f34f15473
Patrick Strasser <strasserpatrick@outlook.de>
2024-01-05 16:03:07 +0100

    docs: add todo

--- src/infra/deployer.py
+++ src/infra/deployer.py
@@ -6,7 +6,9 @@ from infra.docker_stack import DockerStack
 
 if __name__ == "__main__":
     app = App()
+
     AzureStack(app, "azurestack", config=infra_config)
+    # TODO: between azure stack and docker stack, acr registry config needs to be passed
     DockerStack(app, "dockerstack", config=infra_config)
 
     app.synth()
