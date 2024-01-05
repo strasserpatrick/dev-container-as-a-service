e7c1bc1fe91e19534f006d6535e33947b69e4485
Patrick Strasser <strasserpatrick@outlook.de>
2024-01-05 15:16:39 +0100

    wip: add storage account iac code

--- src/infra/azure_stack.py
+++ src/infra/azure_stack.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 
 from cdktf import LocalBackend, TerraformStack
+from cdktf_cdktf_provider_azurerm.kubernetes_cluster import KubernetesCluster, KubernetesClusterDefaultNodePool
 from cdktf_cdktf_provider_azurerm.provider import AzurermProvider
 from cdktf_cdktf_provider_azurerm.resource_group import ResourceGroup
+from cdktf_cdktf_provider_azurerm.storage_account import StorageAccount
+from cdktf_cdktf_provider_azurerm.storage_container import StorageContainer
 from constructs import Construct
 
 from infra.config import InfraConfig
@@ -23,6 +26,13 @@ class AzureStack(TerraformStack):
             location=self.config.azure_location,
         )
 
+        (
+            self.persistent_storage_account,
+            self.persistent_storage_container,
+        ) = self.create_storage()
+
+        # self.kubernetes_cluster = self.create_kubernetes_cluster()
+
     def configure_provider_and_backend(self):
         backend_file_path: Path = self.config.local_backend_path / "azure.tfstate"
 
@@ -36,3 +46,40 @@ class AzureStack(TerraformStack):
         )
 
         AzurermProvider(scope=self, id="azure_stack_provider", features={})
+
+    def create_storage(self):
+        persistent_storage_account = StorageAccount(
+            scope=self,
+            id_="devcontainers_sa",
+            name="perssa4devcontainers",
+            resource_group_name=self.resource_group.name,
+            location=self.config.azure_location,
+            account_replication_type="LRS",
+            account_tier="Standard",
+        )
+
+        persistent_storage_container = StorageContainer(
+            scope=self,
+            id_="devcontainers_sc",
+            name="vscodeuser",
+            storage_account_name=persistent_storage_account.name,
+            container_access_type="private",
+        )
+
+        return persistent_storage_account, persistent_storage_container
+
+    def create_kubernetes_cluster(self):
+        aks_cluster = KubernetesCluster(
+            scope=self,
+            id_="devcontainers_aks",
+            name="aks",
+            resource_group_name=self.config.azure_resource_group_name,
+            location=self.config.azure_location,
+            default_node_pool=KubernetesClusterDefaultNodePool(
+                name="defaultnodepool",
+                vm_size="Standard_D2_v2",
+                node_count=1,
+            ),
+        )
+
+        return aks_cluster
